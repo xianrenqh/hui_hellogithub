94e39aeaf3cb430da5a538e933ef4fee195b716e
hui <xianrenqh@163.com>
2021-12-21 09:50:27 +0800

    update plugins

--- addons/loginbg/config.php
+++ addons/loginbg/config.php
@@ -1,8 +1,8 @@
 <?php
-
 return [
-    'mode' => [
-        'title'   => '模式:',
+    'mode' => array(
+        'name'    => 'mode',
+        'title'   => '模式',
         'type'    => 'radio',
         'options' => array(
             'fixed'  => '固定',
@@ -11,11 +11,12 @@ return [
         ),
         'value'   => 'random',
         'tip'     => '随机切换只支持七天内图片',
-    ],
-    'pic'  => [
+    ),
+    'pic'  => array(
+        'name'  => 'pic',
         'title' => '固定图片',
         'type'  => 'image',
         'value' => '',
         'tip'   => '选择固定则需要上传此图片',
-    ],
+    )
 ];
